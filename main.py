b7d1e14fcd5d8808cfde4f9b9b3b9ad2ca654d45
atrox3d <atrox3d@gmail.com>
2022-12-21 09:25:06 +0100

    day 30: return temperature from station, date reading csv

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -11,13 +13,23 @@ def home():
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    temperature = 23
-    return {
-        "station": station,
-        "date": date,
-        "temperature": temperature
-    }
-
+    try:
+        DATA_DIR = "data_small"
+        filename = f"TG_STAID{station:>06s}.txt"
+        csv_path = Path(DATA_DIR, filename)
+        df = pd.read_csv(str(csv_path), skiprows=20)
+        df.columns = df.columns.str.strip()
+        # temperature = 23
+        temperature = df.loc[df.DATE == int(date)].TG.squeeze() / 10
+        return {
+            "station": station,
+            "date": date,
+            "temperature": str(temperature)
+        }
+    except FileNotFoundError as e:
+        return {
+            "error": f"station {station} does not exist"
+        }
 
 if __name__ == '__main__':
     app.run(debug=True)
